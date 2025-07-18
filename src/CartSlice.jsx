7780cb12ddda72a72aab70c6c135a6f4ae5d71dc
Aarya Bharadwaj <aaryabharadwajvr46@gmail.com>
2025-07-18 12:31:27 -0400

    Implemented removeItem and updateQuantity in CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,19 +3,37 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [] // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+      if (item) {
+        item.quantity++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1
+        });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
 
-    
-    },
-  },
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    }
+  }
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
